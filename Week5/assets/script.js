d5c2ca212ab99b13d13a77132cd88d9aa15fc8a4
Theresa Nheap <theresa.nheap@gmail.com>
2023-06-28 14:06:30 -0700

    Update script.js

--- Week5/assets/script.js
+++ Week5/assets/script.js
@@ -16,49 +16,60 @@ async function fetchWeatherData(city) {
 
 // Function to update weather information on the page
 function updateWeatherInformation(city) {
-  fetchWeatherData(city)
-    .then((data) => {
-      if (data) {
-        const cityNameElement = document.getElementById('city-name');
-        const temperatureElement = document.getElementById('temp-color');
-        const weatherDescriptionElement = document.getElementById('weather-description');
-        const weatherImageElement = document.getElementById('weather-image');
-        const photoGalleryElements = document.getElementsByClassName('photo-gallery');
-
-        let cityDisplayName = '';
-        let weatherIcon = '';
-
-        if (city === 'chandler') {
-          cityDisplayName = 'Chandler🌵☀️';
-          weatherIcon = '01d'; // Example weather icon code for clear sky
-        } else if (city === 'portland') {
-          cityDisplayName = 'Portland🌧️🌲';
-          weatherIcon = '09d'; // Example weather icon code for shower rain
-        } else if (city === 'seattle') {
-          cityDisplayName = 'Seattle🌦️🌲';
-          weatherIcon = '10d'; // Example weather icon code for rain
-        } else if (city === 'saltlake') {
-          cityDisplayName = 'Salt Lake🏞️🏔️';
-          weatherIcon = '13d'; // Example weather icon code for snow
-        } else if (city === 'victor') {
-          cityDisplayName = 'Victor🏔️❄️';
-          weatherIcon = '50d'; // Example weather icon code for mist
+    fetchWeatherData(city)
+      .then((data) => {
+        if (data) {
+          const cityNameElement = document.getElementById('city-name');
+          const temperatureElement = document.getElementById('temp-color');
+          const weatherDescriptionElement = document.getElementById('weather-description');
+          const weatherImageElement = document.getElementById('weather-image');
+          const photoGalleryElements = document.getElementsByClassName('photo-gallery');
+  
+          let cityDisplayName = '';
+          let weatherIcon = '';
+  
+          if (city === 'chandler') {
+            cityDisplayName = 'Chandler🌵☀️';
+            weatherIcon = '01d'; // Example weather icon code for clear sky
+          } else if (city === 'portland') {
+            cityDisplayName = 'Portland🌧️🌲';
+            weatherIcon = '09d'; // Example weather icon code for shower rain
+          } else if (city === 'seattle') {
+            cityDisplayName = 'Seattle🌦️🌲';
+            weatherIcon = '10d'; // Example weather icon code for rain
+          } else if (city === 'saltlake') {
+            cityDisplayName = 'Salt Lake🏞️🏔️';
+            weatherIcon = '13d'; // Example weather icon code for snow
+          } else if (city === 'victor') {
+            cityDisplayName = 'Victor🏔️❄️';
+            weatherIcon = '50d'; // Example weather icon code for mist
+          }
+  
+          cityNameElement.innerHTML = `<strong>${cityDisplayName}</strong>`;
+  
+          if (data.main && data.main.temp) {
+            temperatureElement.innerHTML = `${data.main.temp}°C`;
+          } else {
+            temperatureElement.innerHTML = 'N/A';
+          }
+  
+          if (data.weather && data.weather.length > 0 && data.weather[0].description) {
+            weatherDescriptionElement.textContent = data.weather[0].description;
+          } else {
+            weatherDescriptionElement.textContent = 'Weather information unavailable';
+          }
+  
+          weatherImageElement.setAttribute('src', getWeatherImageURL(weatherIcon));
+  
+          for (let i = 0; i < photoGalleryElements.length; i++) {
+            photoGalleryElements[i].src = `images/${city}-${i + 1}.jpg`;
+          }
         }
-
-        cityNameElement.innerHTML = `<strong>${cityDisplayName}</strong>`;
-        temperatureElement.innerHTML = `${data.main.temp}°C`;
-        weatherDescriptionElement.textContent = data.weather[0].description;
-        weatherImageElement.setAttribute('src', getWeatherImageURL(weatherIcon));
-
-        for (let i = 0; i < photoGalleryElements.length; i++) {
-          photoGalleryElements[i].src = `images/${city}-${i + 1}.jpg`;
-        }
-      }
-    })
-    .catch((error) => {
-      console.log('Error updating weather information:', error);
-    });
-}
+      })
+      .catch((error) => {
+        console.log('Error updating weather information:', error);
+      });
+  }
 
 // Function to get the URL for the weather condition image
 function getWeatherImageURL(iconCode) {
